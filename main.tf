f5c8f5ce3d8da1bef64ac0776c92a6e4d220c60d
Jhonata Silva <jhsp.silva@gmail.com>
2023-09-26 20:02:19 -0300

    ajustes de pastas

--- main.tf
+++ main.tf
@@ -35,9 +35,10 @@ resource "aws_security_group" "security_group_jhonata" {
   }
 }
 
-resource "aws_instance" "jhonata_vm" {
+resource "aws_instance" "jhonata_vm_actions" {
   ami           = "ami-053b0d53c279acc90"  # AMI do Ubuntu 18.04 (substitua pela AMI desejada)
   instance_type = "t2.micro"  # Tipo de instância (substitua pelo tipo desejado)
+  key_name      = aws_key_pair.meu_keypair_jhonata.key_name
 
   vpc_security_group_ids = [aws_security_group.security_group_jhonata.id]
 
@@ -50,9 +51,10 @@ resource "aws_instance" "jhonata_vm" {
               EOF
 
   tags = {
-    Name        = "jhonata_vm"
+    Name        = "jhonata_vm_actions"
     Environment = "dev"
     Application = "backend"
     Class       = "DevOps"
+    Origem      = "Meu primeiro git actions"
   }
 }
