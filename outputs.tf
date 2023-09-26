e97455f51f4bce6aa36a6bbd435f3b1f97dee894
Jhonata Silva <jhsp.silva@gmail.com>
2023-09-26 20:07:50 -0300

    atualizar VM

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "instance_ip" {
-  value = aws_instance.jhonata_vm.public_ip
+  value = aws_instance.jhonata_vm_actions.public_ip
   description = "O IP público da instância EC2"
 }
 output "instance_id" {
-  value = aws_instance.jhonata_vm.id
+  value = aws_instance.jhonata_vm_actions.id
 }
 output "instance_public_dns" {
-  value = aws_instance.jhonata_vm.public_dns
+  value = aws_instance.jhonata_vm_actions.public_dns
 }
 
 
