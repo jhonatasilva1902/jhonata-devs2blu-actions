55d3163607e4e819867153dbe389ca868eaea337
Jhonata Silva <jhsp.silva@gmail.com>
2023-09-26 22:26:38 -0300

    novo grupo e keypair

--- key.tf
+++ key.tf
@@ -1,5 +1,5 @@
-resource "aws_key_pair" "meu_keypair_jhonata" {
+resource "aws_key_pair" "meu_keypair_jhonata_actions" {
   count      = var.create_key ? 1 : 0
-  key_name   = "meu-keypair-jhonata"
+  key_name   = "meu-keypair-jhonata-actions"
   public_key = file("~/.ssh/id_rsa.pub")
 }
--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "security_group_jhonata_actions" {
-  name        = "security-group-jhonata"
+  name        = "security-group-jhonata_actions"
   description = "Security Group para SSH, HTTP e porta 8000"
 
   ingress {
@@ -38,7 +38,7 @@ resource "aws_security_group" "security_group_jhonata_actions" {
 resource "aws_instance" "jhonata_vm_actions" {
   ami           = "ami-053b0d53c279acc90"  # AMI do Ubuntu 18.04 (substitua pela AMI desejada)
   instance_type = "t2.micro"  # Tipo de instância (substitua pelo tipo desejado)
-  key_name      = aws_key_pair.meu_keypair_jhonata.key_name
+  key_name      = aws_key_pair.meu_keypair_jhonata_actions.key_name
 
   vpc_security_group_ids = [aws_security_group.security_group_jhonata_actions.id]
 
@@ -60,7 +60,7 @@ resource "aws_instance" "jhonata_vm_actions" {
     Environment = "dev"
     Application = "backend"
     Class       = "DevOps"
-    Origem      = "Meu primeiro git actions"
+    Origem      = "Meu segundo git actions"
   }
   
 
