904657c2870cdeb5f3eb4d6d27f1d238799b051b
Jhonata Silva <jhsp.silva@gmail.com>
2023-09-27 19:10:14 -0300

    atualizando codigos

--- key.tf
+++ key.tf
@@ -1,5 +1,4 @@
 resource "aws_key_pair" "meu_keypair_jhonata_actions" {
-
   key_name   = "meu-keypair-jhonata-actions"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
