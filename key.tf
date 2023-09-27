aac157fdd6b8f49afec684dc4cead1abda5422eb
Jhonata Silva <jhsp.silva@gmail.com>
2023-09-27 19:42:49 -0300

    Delete keypair

--- key.tf
+++ key.tf
@@ -1,9 +1,6 @@
 resource "aws_key_pair" "meu_keypair_jhonata_actions" {
   key_name   = "meu-keypair-jhonata-actions"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
-  count      = data.aws_key_pair.existing_key ? 0 : 1
+
 }
 
-data "aws_key_pair" "existing_key" {
-  key_name = aws_key_pair.meu_keypair_jhonata_actions[0].key_name
-}
