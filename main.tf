6480434f4919a8f398f5e63a6b1216181a362ef7
Jhonata Silva <jhsp.silva@gmail.com>
2023-09-26 21:58:03 -0300

    ajuste variavel key_name

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "security_group_jhonata" {
 resource "aws_instance" "jhonata_vm_actions" {
   ami           = "ami-053b0d53c279acc90"  # AMI do Ubuntu 18.04 (substitua pela AMI desejada)
   instance_type = "t2.micro"  # Tipo de instância (substitua pelo tipo desejado)
-  key_name      = aws_key_pair.meu_keypair_jhonata.key_name
+
 
   vpc_security_group_ids = [aws_security_group.security_group_jhonata.id]
 
