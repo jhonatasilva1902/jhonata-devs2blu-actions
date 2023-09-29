5c7048df0e623cf24c19171f7b4efb9ef6889e54
Jhonata Silva <jhsp.silva@gmail.com>
2023-09-28 22:16:01 -0300

    test2 key.tf

--- key.tf
+++ key.tf
@@ -1,16 +1,16 @@
-# resource "aws_key_pair" "keypair_jhonata_vm_actions" {
-#   key_name   = "meu-keypair-jhonata-actions"
-#   public_key = "${file("~/.ssh/id_rsa.pub")}"
+resource "aws_key_pair" "keypair_jhonata_vm_actions" {
+  key_name   = "keypair-jhonata-actions"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
 
-# }
+}
 
-# # Data Source para buscar a instância EC2 com base no nome da VM
-# data "aws_instance" "my_instance" {
-#   filter {
-#     name   = "tag:Name"
-#     values = ["jhonata_vm_actions"]
-#   }
-# }
+# Data Source para buscar a instância EC2 com base no nome da VM
+data "aws_instance" "my_instance" {
+  filter {
+    name   = "tag:Name"
+    values = ["jhonata_vm_actions"]
+  }
+}
 
 # # Data Source para buscar a chave SSH com base no nome da keypair
 # data "aws_key_pair" "my_keypair" {
