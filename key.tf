3ae91380a358ed01610d8c4c3f28654e735c0bbc
Jhonata Silva <jhsp.silva@gmail.com>
2023-09-28 21:49:48 -0300

    key.tf atualizado

--- key.tf
+++ key.tf
@@ -15,14 +15,11 @@ data "aws_instance" "my_instance" {
 # Data Source para buscar a chave SSH com base no nome da keypair
 data "aws_key_pair" "my_keypair" {
   key_name = "meu-keypair-jhonata-vm-actions"
+  public_key = data.aws_instance.existing_instance.public_key
+}
 }
 
 # Data Source para buscar o grupo de segurança com base no nome
 data "aws_security_group" "my_security_group" {
   name = "security-group-jhonata_actions"
-}
-
-resource "aws_key_pair" "keypair_jhonata_vm_actions" {
-  key_name   = "meu-keypair-jhonata-vm-actions"
-  public_key = data.aws_instance.existing_instance.public_key
 }
