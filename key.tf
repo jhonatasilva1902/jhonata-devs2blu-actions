c26deb47f5b99ddb1428cfa74a918b1b719509d9
Jhonata Silva <jhsp.silva@gmail.com>
2023-10-02 19:03:54 -0300

    Key teste

--- key.tf
+++ key.tf
@@ -10,7 +10,6 @@ resource "aws_key_pair" "keypair_jhonata_vm_actions" {
   public_key = fileexists("~/.ssh/id_rsa.pub") ? file("~/.ssh/id_rsa.pub") : var.ssh_public_key
 }
 
-
 # # Data Source para buscar a instância EC2 com base no nome da VM
 # data "aws_instance" "my_instance" {
 #   filter {
